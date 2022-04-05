8e0ad85b54ff8db8a4f3114c361baac4eccadffd
Bhoomika Sahu <sahu.bhoomika@tftus.com>
2022-04-05 17:10:35 +0530

    Coding Platform app

---
+++ src/components/App.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import Coding from "./CodingPlatforms";
+
+function App() {
+  return (
+    <>
+      <h1 className="main_heading">Top 5 Coding Platforms</h1>
+      <div className="container">
+        <Coding />
+      </div>
+    </>
+  );
+}
+
+export default App;
